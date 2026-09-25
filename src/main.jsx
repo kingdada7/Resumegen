9cf021fe9ab82af78a93845e9ce31976ed9c2b02
kingdada7 <toluwadada7@gmail.com>
2026-09-25 05:02:31 +0100

    Refactor main.jsx to improve imports and structure, ensuring proper rendering with React.StrictMode and Toaster integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-right" />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 );
